b8ba53f62420c258af05e38d7d158689deb40fc7
austinkret <austinkret@gmail.com>
2021-04-29 01:36:04 +0000

    node postgres assignemnt completed

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,17 +8,19 @@ const pool = new Pool({
 });
 
 const cohortName = process.argv[2];
-const limitTotal = process.argv[3];
 
 pool.query(`
-SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
-FROM students
-JOIN cohorts ON cohort_id=cohorts.id
-LIMIT ${limitTotal};
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = '${cohortName}'
+ORDER BY teacher;
 `)
   .then(res => {
     res.rows.forEach(user => {
-      console.log(`${user.name} has an id of ${user.student_id} and was in the ${cohortName} cohort`);
+      console.log(`${cohortName}: ${user.teacher}`);
     });
   })
   .catch(err => {
